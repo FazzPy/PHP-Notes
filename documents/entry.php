0b816e096060a13cca77cfe3902906b6b1955673
Fazz <97360909+FazzPy@users.noreply.github.com>
2022-10-13 03:19:18 +0300

    Create entry.php

---
+++ documents/entry.php
@@ -0,0 +1,20 @@
+<html>
+ <head>
+<title>Fazz Tech</title>
+ </head>
+ <body>
+
+    <?php
+    $sayi = 10;
+    $yazi = "Bilişim Teknolojileri";
+    $karakter = 'X';
+    echo $sayi;
+    echo '<br/>';
+    echo $yazi;
+    echo '<br/>';
+    echo $karakter;
+    echo '<br/>';
+    ?>
+
+ </body>
+</html>
